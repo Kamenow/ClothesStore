36a923677f1ce0082d2766f1422cf3a46bda270b
Kamenow <71972811+Kamenow@users.noreply.github.com>
2021-11-17 17:03:17 +0200

    added userSchema and Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator/check');
 
-// @route   Get api/users
-// @desc    Test route
+// @route   POST api/users
+// @desc    Register route
 // @access  Public
-router.get('/', (req, res) => {
+router.post('/', [
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Please include a valid email')
+        .isEmail(),
+    check('password', 'Please enter a password with 6 or more charachters')
+        .isLength({ min: 6 })
+], (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        })
+    }
+
     res.send('User Route');
-})
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const PORT = process.env.PORT || 5000;
 // Connect Database
 connectDB();
 
+// Innit Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
     res.send(`<h1>Yo<h1/>`)
 })
