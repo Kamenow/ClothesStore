1aab55c95206846af06659ee95c674a6c52f952f
Kamenow <71972811+Kamenow@users.noreply.github.com>
2021-11-17 16:03:01 +0200

    connect database with mongoose
    connected database with mongoose
    and added config folder with global default values

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db.js')
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => {
     res.send(`<h1>Yo<h1/>`)
