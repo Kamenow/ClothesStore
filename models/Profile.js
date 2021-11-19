bbe13eccd5d1061f646a79b49a4c5fcb4ff146f9
Kamenow <71972811+Kamenow@users.noreply.github.com>
2021-11-19 16:01:33 +0200

    added profile schema

---
+++ models/Profile.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    company: {
+        type: String
+    },
+    location: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    bio: {
+        type: String
+    },
+    social: {
+        youtube: {
+            type: String
+        },
+        twitter: {
+            type: String
+        },
+        facebook: {
+            type: String
+        },
+        linkedin: {
+            type: String
+        },
+        instagram: {
+            type: String
+        },
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
