5e1636eebc91663a34eb291f5e808cfd0939b9b2
Kamenow <71972811+Kamenow@users.noreply.github.com>
2021-11-19 15:21:57 +0200

    custom auth middleware & jwt verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../../models/Users.js');
 
@@ -52,9 +54,22 @@ router.post('/', [
         user.password = await bcrypt.hash(password, salt);
         await user.save();
 
-        //  Return jsonwebtoken
+        const payload = {
+            user: {
+                id: user.id,
+            }
+        }
 
-        res.send('User Registered');
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            { expiresIn: 360000 },
+            (err, token) => {
+                if (err) {
+                    throw err
+                }
+                res.json({ token });
+            });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
