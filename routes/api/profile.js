6bef2775611fa6d9f1ea5688da827870b739ad5a
Kamenow <71972811+Kamenow@users.noreply.github.com>
2021-11-25 16:43:58 +0200

    delete user and profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -127,4 +127,24 @@ router.get('/user/:user_id', async (req, res) => {
     }
 })
 
+// @route   DELETE api/profile
+// @desc    DELETE profile, user & post
+// @access  Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove users posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
