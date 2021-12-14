1170dc0f8a14509fe8f9e3c3002cd193dd231953
Kamenow <71972811+Kamenow@users.noreply.github.com>
2021-12-14 15:40:14 +0200

    added dynamic nabvar

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth.js';
 
-const Navbar = () => {
+const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
+    const authLinks = (
+        <Fragment>
+            <li><Link to="#!" onClick={logout}>Logout</Link></li>
+        </Fragment>
+    );
+
+    const guestLinks = (
+        <Fragment>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+        </Fragment>
+    );
+
     return (
         <div>
             <div className="navbar">
@@ -19,13 +35,24 @@ const Navbar = () => {
                     <ul>
                         <li><Link to="#">Products</Link></li>
                         <li><Link to="#">Contact</Link></li>
-                        <li><Link to="/login">Login</Link></li>
-                        <li><Link to="/register">Register</Link></li>
+                        {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
                     </ul>
                 </div>
             </div>
         </div>
     )
-}
+};
 
-export default Navbar;
+Navbar.propTypes = {
+    logout: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(
+    mapStateToProps,
+    { logout })
+    (Navbar);
