8f131b8bd57d3251dd17c053d45ee7dd9e3d5561
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-03-01 16:57:57 +0200

    fixed db issue

--- config/db.js
+++ config/db.js
@@ -3,17 +3,17 @@ const config = require('config');
 const db = config.get('mongoURI');
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-        });
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+    });
 
-        console.log('MongoDB connected...');
-    } catch (err) {
-        console.log(err.message);
-        //Exit process with failure
-        process.exit(1)
-    }
-}
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.log(err.message);
+    //Exit process with failure
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
