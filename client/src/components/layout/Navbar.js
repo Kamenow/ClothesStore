299834b322005f15fae6285bb27808a44799c2d5
Kamenow <71972811+Kamenow@users.noreply.github.com>
2022-03-01 16:58:33 +0200

    navbar styling update

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,58 +4,77 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { logout } from '../../actions/auth.js';
 
-const Navbar = ({ auth: { isAuthenticated, loading }, profile: { profile }, logout }) => {
-    const authLinks = (
-        <Fragment>
-            <li><Link to="#!" onClick={logout}>Logout</Link></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/create-post">Create Post</Link></li>
-        </Fragment>
-    );
+const Navbar = ({
+  auth: { isAuthenticated, loading },
+  profile: { profile },
+  logout,
+}) => {
+  const authLinks = (
+    <Fragment>
+      <li>
+        <Link to='#!' onClick={logout}>
+          Logout
+        </Link>
+      </li>
+      <li>
+        <Link to='/dashboard'>Dashboard</Link>
+      </li>
+      <li>
+        <Link to='/create-post'>Create Post</Link>
+      </li>
+    </Fragment>
+  );
 
-    const guestLinks = (
-        <Fragment>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/register">Register</Link></li>
-        </Fragment>
-    );
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+    </Fragment>
+  );
 
-    return (
-        <div>
-            <div className="navbar">
-                <div className="icon">
-                    <h1 className="logo" >
-                        <Link to="/"
-                            style={{
-                                textDecoration: 'none',
-                                color: 'brown'
-                            }}>Yavor Store</Link>
-                    </h1>
-                </div>
+  return (
+    <div className='navbar'>
+      <div className='icon'>
+        <h1 className='logo'>
+          <Link
+            to='/'
+            style={{
+              textDecoration: 'none',
+              color: 'brown',
+            }}
+          >
+            Yavor Store
+          </Link>
+        </h1>
+      </div>
 
-                <div className="menu">
-                    <ul>
-                        <li><Link to="/posts">Products</Link></li>
-                        {!loading && (<Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>)}
-                    </ul>
-                </div>
-            </div>
-        </div>
-    )
+      <div className='menu'>
+        <ul>
+          <li>
+            <Link to='/posts'>Products</Link>
+          </li>
+          {!loading && (
+            <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 Navbar.propTypes = {
-    logout: PropTypes.func.isRequired,
-    auth: PropTypes.object.isRequired,
-    profile: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
 };
 
-const mapStateToProps = state => ({
-    auth: state.auth,
-    profile: state.profile
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
 });
 
-export default connect(
-    mapStateToProps,
-    { logout })
-    (Navbar);
+export default connect(mapStateToProps, { logout })(Navbar);
